abdb8da8123947895ca02fa48e67e2c7647f0ac6
Roma <romakoder@gmail.com>
2020-02-11 23:46:27 +0200

    Updates

---
+++ precache-manifest.56ac1535cdd635eeb468a1776f7c3205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f4d3e0c4bd5706e6ce03ea84e4a14e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea210ca5cb5c01e900fd",
+    "url": "./static/css/main.83f799c7.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea210ca5cb5c01e900fd",
+    "url": "./static/js/main.3a3f165e.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
